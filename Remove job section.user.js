654652d5c696acac816a611b42bd9a5fb8730242
Shree Khanal <shree@braindigit.com>
2018-08-20 12:26:19 +0545

    Hide job section v1

---
+++ Remove job section.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Remove job section
+// @namespace    https://github.com/wimglenn/userscripts/
+// @version      0.1
+// @description  Hides the job section
+// @author       Shree: https://stackoverflow.com/users/965146/shree
+// @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/questions/.*
+// @grant        none
+// ==/UserScript==
+
+function removehireme() {
+    "use strict";
+    var bs = document.getElementById("hireme")
+    bs.remove();
+};
+
+$(document).ready(function() {
+  removehireme();
+});
