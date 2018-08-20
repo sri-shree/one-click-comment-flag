f78332330a1355a42ed8df3ce8fb1dcde872d784
Shree Khanal <shree@braindigit.com>
2018-08-20 12:50:37 +0545

    Change url link

--- Remove job section.user.js
+++ Remove job section.user.js
@@ -1,11 +1,18 @@
 // ==UserScript==
 // @name         Hide job section
-// @namespace    https://github.com/sri-shree/Remove-job-section-from-SO-site/raw/master/Remove%20job%20section.user.js
+// @namespace    https://github.com/sri-shree/Remove-job-section-from-SO-site
 // @version      0.1
 // @description  Hide job section
 // @author       Shree: https://stackoverflow.com/users/965146/shree
-// @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/questions/.*
-// @grant        none
+// @grant          none
+// @include /^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
+// @include /^https?:\/\/(.*\.)?serverfault\.com/.*$/
+// @include /^https?:\/\/(.*\.)?superuser\.com/.*$/
+// @include /^https?:\/\/(.*\.)?stackexchange\.com/.*$/
+// @include /^https?:\/\/(.*\.)?askubuntu\.com/.*$/
+// @include /^https?:\/\/(.*\.)?mathoverflow\.net/.*$/
+// @include /^https?:\/\/discuss\.area51\.stackexchange\.com/.*$/
+// @include /^https?:\/\/stackapps\.com/.*$/
 // ==/UserScript==
 
 function removehireme() {
